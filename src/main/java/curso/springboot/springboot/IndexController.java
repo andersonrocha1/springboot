8fd2c24425432fdbc1c69a2c36040ddfeea415c7
Anderson Rocha <anderson.tec.informacao@gmail.com>
2020-10-09 12:28:34 -0300

    Acertando redirecionamento

--- src/main/java/curso/springboot/springboot/IndexController.java
+++ src/main/java/curso/springboot/springboot/IndexController.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	
-	@RequestMapping("/")
-	public String index() {
-	 return "index";	
-	}
-	
 	@RequestMapping("/login")
 	public String login() {
 	 return "login";	
+	}
+	@RequestMapping("/")
+	public String index() {
+		return "index";	
 	}
 
 }
