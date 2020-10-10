014002006f362646516cde2dc0e5262dbba8fbf1
Anderson Rocha <anderson.tec.informacao@gmail.com>
2020-10-10 18:39:03 -0300

    Teste /login

--- src/main/java/curso/springboot/springboot/IndexController.java
+++ src/main/java/curso/springboot/springboot/IndexController.java
@@ -1,4 +1,4 @@
-package curso.springboot.controller;
+package curso.springboot.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
